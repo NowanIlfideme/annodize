6643dd366b83ec86763977a2e35793cfa9d05ce2
NowanIlfideme <git@nowan.dev>
2022-02-10 22:48:25 +0100

    Start from scratch...

--- annodize/__init__.py
+++ annodize/__init__.py
@@ -1,17 +1,5 @@
 """Dataframe validation using PEP 593 (Python 3.9+) Annotated Types."""
 
-__all__ = [
-    "__version__",
-    "BaseHook",
-    "CompositeHook",
-    "Annotated",
-    "Callable",
-    "P_ORIGINAL",
-    "P_HOOKED",
-    "RT_ORIGINAL",
-    "RT_HOOKED",
-]
-
 # TODO: Consider using typing_extensions, future-typing backports
 # https://github.com/PrettyWood/future-typing
 
@@ -19,14 +7,4 @@ __all__ = [
 # https://github.com/PrettyWood/typingx
 # just set this encoding comment at the top of your scripts: # -*- coding: future_typing -*-
 
-from .core import (
-    P_HOOKED,
-    P_ORIGINAL,
-    RT_HOOKED,
-    RT_ORIGINAL,
-    Annotated,
-    BaseHook,
-    Callable,
-    CompositeHook,
-)
 from .version import __version__
