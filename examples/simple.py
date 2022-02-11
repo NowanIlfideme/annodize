f9e9fee9113ae41298a718c1a34db0976b18e16b
NowanIlfideme <git@nowan.dev>
2022-02-11 01:01:41 +0100

    Added simple example.

---
+++ examples/simple.py
@@ -0,0 +1,21 @@
+"""Simple example."""
+
+import pandas as pd
+
+from annodize.core.wrapper import AnnodizeWrapper
+from annodize.ext.typing import Annotated
+
+DO_RUNTIME_CHECK = False
+
+if DO_RUNTIME_CHECK:
+    wrap = AnnodizeWrapper()  # no-op wrapper
+else:
+    wrap = AnnodizeWrapper()  # TODO: add something here
+
+
+@wrap
+def foo(
+    x: Annotated[pd.DataFrame, "foo"], val: int | float = 3
+) -> Annotated[pd.DataFrame, "bar"]:
+    """Example function that is wrapped."""
+    return x.assign(**{"bar": x["foo"] * val})
