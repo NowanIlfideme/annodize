5ed4c894e70d4351be673cc637ca66e19d4c8302
NowanIlfideme <git@nowan.dev>
2022-02-16 00:31:07 +0100

    Bit of cleanup.

--- annodize/_field.py
+++ annodize/_field.py
@@ -3,10 +3,9 @@
 import warnings
 from inspect import Parameter, isclass
 from keyword import iskeyword, issoftkeyword
-from typing import Annotated, Any, TypeGuard, final, get_args, get_origin
+from typing import Annotated, Any, ForwardRef, TypeGuard, final, get_args, get_origin
 
-_AnnType = type(Annotated[int, 1])  # is technically typing._AnnotatedAlias
-TypeLike = type
+TypeLike = type | ForwardRef
 
 
 @final
@@ -42,11 +41,29 @@ def split_annotation(ann: Any) -> tuple[TypeLike, tuple[Any, ...]]:
     return type_, args
 
 
-class RawField(object):
+def enforce_type(type_: TypeLike | str) -> TypeLike:
+    """Ensures that strings are converted at least into `ForwardRef`."""
+    if isinstance(type_, str):
+        return ForwardRef(type_)
+    return type_
+
+
+def repr_type_aware(x: Any) -> str:
+    """Clean(er) representation of the type."""
+    if isinstance(x, type):
+        return x.__qualname__
+    else:
+        return repr(x)
+
+
+@final
+class Field(object):
     """Basic field definition."""
 
     NoDefault = _NoDefault
     __slots__ = ("__name", "__type_", "__ann_args", "__default")
+
+    # Initializers
 
     def __init__(
         self,
@@ -56,22 +73,25 @@ class RawField(object):
         default: Any = NoDefault,
     ):
         self.__name = as_field_name(name)
-        if is_annotated(type_):
+        t = enforce_type(type_)
+        if is_annotated(t):
             if len(ann_args) > 0:
-                raise TypeError(f"Cannot pass `ann_args` with Annotated type {type_!r}")
-            type_, ann_args = split_annotation(type_)
-        self.__type_ = type_
+                raise TypeError(f"Cannot pass `ann_args` with Annotated type {t!r}")
+            t, ann_args = split_annotation(t)
+        self.__type_ = t
         self.__ann_args = tuple(ann_args)
         self.__default = default  # TODO: Copy this value?
 
     @classmethod
-    def from_inspect_parameter(cls, param: Parameter) -> "RawField":
+    def from_inspect_parameter(cls, param: Parameter) -> "Field":
         """Creates this class from an `inspect.Parameter`."""
         if param.default is Parameter.empty:
             default = _NoDefault
         else:
             default = param.default
         return cls(param.name, param.annotation, default=default)
+
+    # Read-only fields
 
     @property
     def name(self) -> str:
@@ -89,16 +109,36 @@ class RawField(object):
         return tuple(self.__ann_args)
 
     @property
+    def default(self) -> Any:
+        """The default value."""
+        return self.__default
+
+    # Dunder methods
+
+    def __repr__(self) -> str:
+        cn = type(self).__qualname__
+        args: list[Any] = [self.__name, self.__type_]
+        if self.has_args or self.has_default:
+            args.append(self.__ann_args)
+        if self.has_default:
+            args.append(self.__default)
+        return cn + "(" + ", ".join([repr_type_aware(x) for x in args]) + ")"
+
+    # Helper properties
+
+    @property
+    def has_forward_ref(self) -> bool:
+        """Whether the internal type is a `ForwardRef`."""
+        return isinstance(self.__type_, ForwardRef)
+
+    @property
     def has_args(self) -> bool:
         """Whether this field has any annotation arguments."""
         return len(self.__ann_args) > 0
 
     @property
-    def default(self) -> Any:
-        """The default value."""
-        return self.__default
-
-    @property
     def has_default(self) -> bool:
         """Whether this field has any default value."""
         return self.__default is not _NoDefault
+
+    # TODO: some way to 'finalize' ForwardRef, similar to how Pydantic does it
