ea37df141816c90b4c4586bfeec218df2ba4ab22
NowanIlfideme <git@nowan.dev>
2022-10-03 21:11:59 +0200

    Fixes in plugins.

--- annodize/dataframe/example/plugin.py
+++ annodize/dataframe/example/plugin.py
@@ -6,7 +6,9 @@ Note that this is specified in the `entry_points` of your package.
 
 def col_major():
     """Column-major plugin entry point."""
+    print("col major loaded")
 
 
 def row_major():
     """Row-major plugin entry point."""
+    print("row major loaded")
--- annodize/dataframe/plugin_loader.py
+++ annodize/dataframe/plugin_loader.py
@@ -1,1 +1,20 @@
 """Loads `annodize.dataframe` plugins."""
+
+import importlib.metadata as _im
+import warnings
+from typing import Callable
+
+
+def load_plugins():
+    """Loads all plugins."""
+    eps: _im.EntryPoints = _im.entry_points(group="annodize.dataframe")
+    for ep_i in eps:
+        try:
+            func: Callable = ep_i.load()
+            func()
+        except Exception:
+            warnings.warn(f"Failed to load plugin for: {ep_i.name!r}")
+
+
+if __name__ == "__main__":
+    load_plugins()
