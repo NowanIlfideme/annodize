4a4f8e0a5b35fe582a32213959c0c85663ca4bb9
NowanIlfideme <git@nowan.dev>
2022-10-03 22:53:16 +0200

    Added Field (suspiciously similar to my old one...)

--- annodize/dataframe/schema.py
+++ annodize/dataframe/schema.py
@@ -1,22 +1,61 @@
 """Defining a (possibly recursive) schema."""
 
-from typing import Callable, TypeVar
+from typing import Any, Callable, TypeVar
 
 DF = TypeVar("DF")
 """Generic dataframe type."""
 
 
-class DFValidationError(ValueError):
-    """Dataframe validation failed (one or more errors)."""
+class Field:
+    """Dataframe field definition."""
 
-    # TODO: Location, wrap multiple errors, etc.
+    __slots__ = "__name", "__type", "__default", "__extra_annotations"
+
+    NO_DEFAULT = ...
+
+    def __init__(
+        self,
+        name: str,
+        type_: type,
+        default: Any = NO_DEFAULT,
+        extra_annotations: list[Any] = [],
+    ):
+        self.__name = name
+        self.__type = type_
+        self.__default = default
+        self.__extra_annotations = list(extra_annotations)
+
+    @classmethod
+    def from_annotation(cls, ann: str | Any) -> "Field":
+        """Gets the field from the annotation."""
+        # TODO: Make sure to support string-type annotations... hacky I guess.
+        raise NotImplementedError("TODO.")
+
+    @property
+    def name(self) -> str:
+        """The name of the field."""
+        return self.__name
+
+    @property
+    def type_(self) -> type:
+        """The type of the field."""
+        return self.__type
+
+    @property
+    def default(self) -> Any:
+        """The default value of the field."""
+        return self.__default
+
+    @property
+    def extra_annotations(self) -> list[Any]:
+        return list(self.__extra_annotations)
 
 
 class Schema:
     """Dataframe schema definition."""
 
     @classmethod
-    def validate(cls, df: DF) -> DFValidationError | None:
+    def validate(cls, df: DF) -> "DFValidationError" | None:
         """Validates the dataframe, returning errors."""
         from .api import plugin_manager
 
@@ -41,6 +80,17 @@ class Schema:
     def coerce(cls, df: DF) -> DF:
         """Coerces the dataframe to conform to the schema, if possible."""
         raise NotImplementedError("Not yet implemented at all.")
+
+    @classmethod
+    def fields(cls) -> list[Field]:
+        """Returns all the fields of this class."""
+        raise NotImplementedError("TODO.")
+
+
+class DFValidationError(ValueError):
+    """Dataframe validation failed (one or more errors)."""
+
+    # TODO: Location, wrap multiple errors, etc.
 
 
 InstanceCheck = Callable[[DF], bool]
